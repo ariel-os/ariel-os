77a6350242a04a728cecaed7cf956143dc2d24fd
Kaspar Schleiser <kaspar@schleiser.de>
2025-02-13 21:58:39 +0100

    fixup! refactor(embassy): move thread executor config into `-common`

--- src/ariel-os-embassy-common/src/executor_thread.rs
+++ src/ariel-os-embassy-common/src/executor_thread.rs
@@ -7,7 +7,8 @@ pub const STACKSIZE: usize = ariel_os_utils::usize_from_env_or!(
     "executor thread stack size"
 );
 
-pub(crate) const PRIORITY: u8 = ariel_os_utils::u8_from_env_or!(
+/// Priority used by main executor thread.
+pub const PRIORITY: u8 = ariel_os_utils::u8_from_env_or!(
     "CONFIG_EXECUTOR_THREAD_PRIORITY",
     8,
     "executor thread priority"
