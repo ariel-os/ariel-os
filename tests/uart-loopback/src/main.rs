dc574336cac4beeffab49b6b50d94a68da31c0c4
Koen Zandberg <koen@bergzand.net>
2025-04-08 15:39:53 +0200

    feat: Add UART loopback test
    Signed-off-by: Koen Zandberg <koen@bergzand.net>

---
+++ tests/uart-loopback/src/main.rs
@@ -0,0 +1,58 @@
+//! This is a test for UART loopback operation
+
+#![no_main]
+#![no_std]
+#![feature(type_alias_impl_trait)]
+#![feature(used_with_arg)]
+#![feature(impl_trait_in_assoc_type)]
+
+mod pins;
+
+use ariel_os::{
+    debug::{
+        ExitCode, exit,
+        log::{debug, info},
+    },
+    gpio, hal,
+    time::Delay,
+};
+
+use embedded_hal_async::delay::DelayNs;
+use embedded_io_async::Read;
+use embedded_io_async::Write;
+
+#[ariel_os::task(autostart, peripherals)]
+async fn main(peripherals: pins::Peripherals) {
+    Delay {}.delay_ms(500).await;
+    info!("Starting UART test");
+    let mut config = hal::uart::Config::default();
+    config.baudrate = 9600;
+    let mut rx_buf: [u8; 32] = [0u8; 32];
+    let mut tx_buf: [u8; 32] = [0u8; 32];
+    info!("Selected baud rate: {}", config.baudrate);
+    let mut uart = pins::TestUart::new(
+        peripherals.uart_rx,
+        peripherals.uart_tx,
+        &mut rx_buf,
+        &mut tx_buf,
+        config,
+    );
+
+    const out: &str = &"Test Message";
+    let mut in_ = [0u8; out.len()];
+
+    let _ = uart.write_all(out.as_bytes()).await;
+    let _ = uart.flush().await;
+    info!("written bytes");
+    let _ = uart.read(&mut in_).await;
+
+    info!("got: '{:x}'", &in_);
+
+    assert_eq!(out.as_bytes(), in_);
+
+    info!("Test passed!");
+
+    Delay {}.delay_ms(500).await;
+
+    exit(ExitCode::SUCCESS);
+}
