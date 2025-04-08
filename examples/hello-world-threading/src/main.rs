44106bdb4996278eb253dd64383e4785e40a364e
Kaspar Schleiser <kaspar@schleiser.de>
2025-04-08 14:42:46 +0200

    DON'T MERGE testing code

--- examples/hello-world-threading/src/main.rs
+++ examples/hello-world-threading/src/main.rs
@@ -3,9 +3,48 @@
 
 use ariel_os::debug::{ExitCode, exit, log::*};
 
+use ariel_os::rt::stack::*;
+
 #[ariel_os::thread(autostart)]
 fn main() {
-    info!("Hello World!");
+    let limits = Stack::get();
+    let free_min = limits.free_min();
+    let used_max = limits.used_max();
+
+    info!(
+        "size: {} used: {} free: {} free_min: {} used_max: {}",
+        limits.size(),
+        limits.used(),
+        limits.free(),
+        free_min,
+        used_max,
+    );
+
+    limits.repaint();
+
+    let free_min = limits.free_min();
+    let used_max = limits.used_max();
+
+    info!(
+        "size: {} used: {} free: {} free_min: {} used_max: {}",
+        limits.size(),
+        limits.used(),
+        limits.free(),
+        free_min,
+        used_max,
+    );
+
+    let free_min = limits.free_min();
+    let used_max = limits.used_max();
+
+    info!(
+        "size: {} used: {} free: {} free_min: {} used_max: {}",
+        limits.size(),
+        limits.used(),
+        limits.free(),
+        free_min,
+        used_max,
+    );
 
     exit(ExitCode::SUCCESS);
 }
--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -3,9 +3,49 @@
 
 use ariel_os::debug::{ExitCode, exit, log::*};
 
+use ariel_os::rt::stack::*;
+
 #[ariel_os::task(autostart)]
 async fn main() {
-    info!("Hello World!");
+    let stack = Stack::get();
+
+    let free_min = stack.free_min();
+    let used_max = stack.used_max();
+
+    info!(
+        "size: {} used: {} free: {} free_min: {} used_max: {}",
+        stack.size(),
+        stack.used(),
+        stack.free(),
+        free_min,
+        used_max,
+    );
+
+    stack.repaint();
+
+    let free_min = stack.free_min();
+    let used_max = stack.used_max();
+
+    info!(
+        "size: {} used: {} free: {} free_min: {} used_max: {}",
+        stack.size(),
+        stack.used(),
+        stack.free(),
+        free_min,
+        used_max,
+    );
+
+    let free_min = stack.free_min();
+    let used_max = stack.used_max();
+
+    info!(
+        "size: {} used: {} free: {} free_min: {} used_max: {}",
+        stack.size(),
+        stack.used(),
+        stack.free(),
+        free_min,
+        used_max,
+    );
 
     exit(ExitCode::SUCCESS);
 }
