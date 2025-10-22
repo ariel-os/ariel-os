20a2b373633b907dbb02450aed33587d5c77b25a
chrysn <chrysn@fsfe.org>
2025-10-22 18:33:23 +0200

    fixup! chore: update generated files for board nrf9151-dk

--- src/ariel-os-boards/src/nrf9151-dk.rs
+++ src/ariel-os-boards/src/nrf9151-dk.rs
@@ -3,10 +3,10 @@
 pub mod pins {
     use ariel_os_hal::hal::peripherals;
     ariel_os_hal::define_peripherals!(
-        LedPeripherals { led1 : P0_00, led2 : P0_01, led3 : P0_04, led4 : P0_05, }
+        LedPeripherals { led0 : P0_00, led1 : P0_01, led2 : P0_04, led3 : P0_05, }
     );
     ariel_os_hal::define_peripherals!(
-        ButtonPeripherals { button1 : P0_08, button2 : P0_09, button3 : P0_18, button4 :
+        ButtonPeripherals { button0 : P0_08, button1 : P0_09, button2 : P0_18, button3 :
         P0_19, }
     );
 }
