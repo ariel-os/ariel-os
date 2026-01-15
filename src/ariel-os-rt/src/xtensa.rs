e53ce0c7b70c660c4257a80b98fee67cc03b6de9
Kaspar Schleiser <kaspar@schleiser.de>
2026-01-15 13:32:53 +0100

    fix(rt): disable esp watchdog in init()
    Signed-off-by: Kaspar Schleiser <kaspar@schleiser.de>

--- src/ariel-os-rt/src/xtensa.rs
+++ src/ariel-os-rt/src/xtensa.rs
@@ -7,7 +7,30 @@ fn main() -> ! {
     crate::startup();
 }
 
-pub fn init() {}
+pub fn init() {
+    disable_watchdog();
+}
+
+// Disables the esp watchdogs.
+//
+// `esp-hal::init()` actually does this already, but our initial stack painting needs to run before
+// that, and with some log messages before that, it takes enough time to trigger the watchdog and
+// reboot.
+fn disable_watchdog() {
+    ariel_os_debug::log::debug!("ariel-os-rt: disabling watchdog timers");
+    // RTC domain must be enabled before we try to disable
+    // SAFETY: creating an LPWR instance out of thin air here, dropping it later.
+    let mut rtc = unsafe { esp_hal::rtc_cntl::Rtc::new(esp_hal::peripherals::LPWR::steal()) };
+
+    // Disable watchdog timers
+    #[cfg(not(any(context = "esp32", context = "esp32s2")))]
+    rtc.swd.disable();
+
+    rtc.rwdt.disable();
+
+    esp_hal::timer::timg::Wdt::<esp_hal::peripherals::TIMG0<'static>>::new().disable();
+    esp_hal::timer::timg::Wdt::<esp_hal::peripherals::TIMG1<'static>>::new().disable();
+}
 
 /// Returns the current stack pointer register value
 pub(crate) fn sp() -> usize {
