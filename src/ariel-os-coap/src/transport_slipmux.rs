5983c7c5212694e17f6139007aa892483e3dbad0
chrysn <chrysn@fsfe.org>
2026-02-23 00:10:14 +0100

    refactor(coap,slipmux): run slipmux in UART task
    This avoids the weird autostart-just-to-store-the-periphs tasks, but
    more importantly also makes slipmux something that can treat CoAP and
    SLIP both as optional components.
    
    Signed-off-by: chrysn <chrysn@fsfe.org>

--- src/ariel-os-coap/src/transport_slipmux.rs
+++ src/ariel-os-coap/src/transport_slipmux.rs
@@ -2,42 +2,17 @@
 // address semantics).
 
 use ariel_os_debug::log::{Hex, debug, warn};
-use embassy_sync::once_lock::OnceLock;
+use embassy_sync::signal::Signal;
 
 mod over_uart {
-    use ariel_os_embassy::cell::SameExecutorCell;
     use static_cell::ConstStaticCell;
 
     use super::*;
 
     type UartAssignment = ariel_os_boards::pins::HOST_FACING_UART;
 
-    // FIXME There has to be a better idiom for a function deep down to take peripherals that does
-    // not involve spawning a task that terminates at startup?
-    //
-    // (Therefore, not bothering with optimizing any of its access)
-    static TAKE_THIS: OnceLock<SameExecutorCell<core::cell::RefCell<Option<UartAssignment>>>> =
-        OnceLock::new();
-
     #[ariel_os_macros::task(autostart, peripherals)]
-    async fn pass_on_uart(peripherals: UartAssignment) {
-        TAKE_THIS
-            .init(SameExecutorCell::new_async(Some(peripherals).into()).await)
-            .ok()
-            .expect("Autostart is only called once");
-    }
-
-    // FIXME: Move to BufRead once <https://github.com/ariel-os/ariel-os/pull/1600> is resolved.
-    pub(super) async fn take() -> impl embedded_io_async::Read + embedded_io_async::Write {
-        let peripherals: UartAssignment = TAKE_THIS
-            .try_get()
-            .unwrap()
-            .get_async()
-            .await
-            .unwrap()
-            .take()
-            .unwrap();
-
+    async fn pass_on_uart(peripherals: UartAssignment) -> ! {
         static RX_BUF: ConstStaticCell<[u8; 16]> = ConstStaticCell::new([0; 16]);
         static TX_BUF: ConstStaticCell<[u8; 16]> = ConstStaticCell::new([0; 16]);
         let rx_buf = RX_BUF.take();
@@ -49,13 +24,52 @@ mod over_uart {
         use ariel_os_hal::uart::Assignment;
         let (tx, rx) = peripherals.into_pins();
 
-        <UartAssignment as Assignment>::Device::new(rx, tx, rx_buf, tx_buf, config)
-            .expect("Invalid UART configuration")
+        let uart = <UartAssignment as Assignment>::Device::new(rx, tx, rx_buf, tx_buf, config)
+            .expect("Invalid UART configuration");
+
+        serve_slipmux(uart).await
     }
 }
 
-pub(crate) async fn coap_run_slipmux(mut handler: impl coap_handler::Handler) -> ! {
-    use embedded_io_async::{Read, Write};
+/// Signal in which [`server_slipmux`] places the decoder buffer if it contains anything which CoAP
+/// should process. The buffer is returned through `COAP2SLIPMUX` to avoid that the task that
+/// places the buffer takes it back immediately without waiting for the other task.
+///
+/// For optimization, we could look into whether there is a way to yield to that other task, but
+/// then we'd have to handle the case when that other task is not active yet, and just
+/// yielding-for-everyone-else still returns us the unmodified buffer.
+///
+/// As there is only a single `&'static mut SingleFrameDecoder`, Signal's overwrite semantics don't
+/// concern us: We don't have anything to overwrite the latest value with, unless it was taken out
+/// and returned before.
+static SLIPMUX2COAP: Signal<
+    embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex,
+    &'static mut SingleFrameDecoder,
+> = Signal::new();
+/// See [`SLIPMUX2COAP`]; this works the same, just in the opposite direction.
+static COAP2SLIPMUX: Signal<
+    embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex,
+    &'static mut SingleFrameDecoder,
+> = Signal::new();
+
+/// Decodes frames from a slipmux peer, and sends them to the relevant tasks for further
+/// processing, receiving frames in return.
+///
+/// In particular, whenever a CoAP frame is ready, it is signaled in [`SLIPMUX2COAP`] (to be
+/// picked up by [`coap_run_slipmux()`], however that is called (autostarted or by the user once
+/// the handler is ready), and this task is then idle until the CoAP response is returned through
+/// [`COAP2SLIPMUX`].
+///
+/// When this module starts doing SLIP as well, the task will treat the network stack as a
+/// signaling peer just like the CoAP stack.
+///
+/// This is auto-started by whichever module provides the slipmux back-end from that module's
+/// autostart task, and…
+///
+/// # Panics
+///
+/// … if called more than once.
+async fn serve_slipmux(mut uart: impl embedded_io_async::Read + embedded_io_async::Write) -> ! {
     use slipmux::DecodeStatus;
 
     // For the time being we play the simple game, where we just have a server, and can afford to
@@ -64,10 +78,10 @@ pub(crate) async fn coap_run_slipmux(mut handler: impl coap_handler::Handler) ->
     // This will not be sustainable with the next generation coap-handler, and then we may need to
     // split the UARTs.
 
-    let mut uart = over_uart::take().await;
+    static SLIPMUX: static_cell::StaticCell<SingleFrameDecoder> = static_cell::StaticCell::new();
+    let mut slipmux = SLIPMUX.init_with(Default::default);
 
     let mut decoder = slipmux::Decoder::new();
-    let mut slipmux = SingleFrameDecoder::default();
     loop {
         // FIXME: This is an ugly reading mechanism, made just slightly more tolerable by
         // decoder.decode() taking things bytewise anyway.
@@ -89,7 +103,7 @@ pub(crate) async fn coap_run_slipmux(mut handler: impl coap_handler::Handler) ->
         //BR consume += 1;
 
         // Ignoring the return value; handling everything inside the decoder.
-        match decoder.decode(byte, &mut slipmux) {
+        match decoder.decode(byte, slipmux) {
             Err(_) => {
                 warn!("Decoding error; trying at the next byte.");
                 //BR consume = 1;
@@ -124,31 +138,18 @@ pub(crate) async fn coap_run_slipmux(mut handler: impl coap_handler::Handler) ->
         //BR uart.consume(consume);
 
         if coap_ready {
-            if slipmux.data().is_err() {
-                warn!("Ignoring overly long CoAP request.");
-                // FIXME: Respond accordingly (Request Entity Too Large and size1)
-                continue;
-            };
-            // Reaching into raw value rather than through accessor because we need it mutable for
-            // the pseudostack.
-            let buffer = &mut slipmux.buffer;
-            // Compensate for checksum -- FIXME: where should this best be done?
-            let Some(actual_length) = buffer.len().checked_sub(2) else {
-                warn!("Ignoring overly short CoAP request.");
-                continue;
-            };
-            buffer.truncate(actual_length);
+            // If SLIP support is added, and CoAP becomes optional, this might detect at build time
+            // that there is no CoAP that we should wait (indefinitely) for, and instead just send
+            // the courtesy RSTs.
+            //
+            // If setting up the CoAP stack ever takes long (and maybe will even deadlock with
+            // networking), this might start issuing RSTs (meh: peer might think they're permanent)
+            // or 5.03 Max-Age: 1 responses.
 
-            debug!("Sending into CoAP stack: {}", Hex(&buffer));
-            let mut pseudostack = PseudoStack(buffer);
-            embedded_nal_minimal_coapserver::poll(
-                &mut pseudostack,
-                &mut PseudoSocket,
-                &mut handler,
-            )
-            .unwrap(); // No `let Ok(()) = `, because there's the unavoidable nb::Error case.
+            SLIPMUX2COAP.signal(slipmux);
+            slipmux = COAP2SLIPMUX.wait().await;
 
-            debug!("Taking out of CoAP stack: {}", Hex(&buffer));
+            let buffer = &slipmux.buffer;
 
             if buffer.len() == 0 {
                 // FIXME: Send RST if parsable to that point (or make
@@ -167,6 +168,37 @@ pub(crate) async fn coap_run_slipmux(mut handler: impl coap_handler::Handler) ->
                 }
             }
         }
+    }
+}
+
+pub(crate) async fn coap_run_slipmux(mut handler: impl coap_handler::Handler) -> ! {
+    loop {
+        let slipmux: &'static mut SingleFrameDecoder = SLIPMUX2COAP.wait().await;
+
+        if slipmux.data().is_err() {
+            warn!("Ignoring overly long CoAP request.");
+            // FIXME: Respond accordingly (Request Entity Too Large and size1)
+            continue;
+        };
+        // Reaching into raw value rather than through accessor because we need it mutable for
+        // the pseudostack.
+        let buffer = &mut slipmux.buffer;
+        // Compensate for checksum -- FIXME: where should this best be done?
+        let Some(actual_length) = buffer.len().checked_sub(2) else {
+            warn!("Ignoring overly short CoAP request.");
+            continue;
+        };
+        buffer.truncate(actual_length);
+
+        debug!("Sending into CoAP stack: {}", Hex(&buffer));
+        let mut pseudostack = PseudoStack(buffer);
+        embedded_nal_minimal_coapserver::poll(&mut pseudostack, &mut PseudoSocket, &mut handler)
+            // No `let Ok(()) = `, because there's the unavoidable nb::Error case.
+            .unwrap();
+
+        debug!("Taking out of CoAP stack: {}", Hex(&buffer));
+
+        COAP2SLIPMUX.signal(slipmux);
     }
 }
 
