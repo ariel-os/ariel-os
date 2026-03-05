75fe400e95b7e16b3ad61685d2ec1dbf0bf87b93
ROMemories <152802150+ROMemories@users.noreply.github.com>
2026-03-05 15:28:40 +0100

    feat(power): expose the reset reason on nRF
    Signed-off-by: ROMemories <152802150+ROMemories@users.noreply.github.com>

--- src/ariel-os-power/src/lib.rs
+++ src/ariel-os-power/src/lib.rs
@@ -6,3 +6,14 @@
 mod reset;
 
 pub use reset::*;
+
+/// Initializes power management.
+///
+/// *Important*: this needs to be called as early as possible in the boot sequence.
+/// In particular, on microcontrollers whose reset reason needs to be cleared manually on each
+/// reset, this needs to be called before anything else has the change to clear it.
+/// This function may clear these bits.
+#[doc(hidden)]
+pub fn init() {
+    reset::save_reset_reason();
+}
