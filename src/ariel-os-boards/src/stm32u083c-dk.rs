ae498862c0de95d5985adfc5fb5828720458ea1f
Antoine Lavandier <antoine.lavandier@inria.fr>
2026-08-06 13:40:57 +0200

    feat(sbd): use 0.4.1 schema version
    Signed-off-by: Antoine Lavandier <antoine.lavandier@inria.fr>

--- src/ariel-os-boards/src/stm32u083c-dk.rs
+++ src/ariel-os-boards/src/stm32u083c-dk.rs
@@ -5,6 +5,10 @@ pub mod pins {
         LedPeripherals { led0 : PC13, led1 : PA5, led2 : PB2, }
     );
     ariel_os_hal::define_peripherals!(ButtonPeripherals { button0 : PC2, });
+    ariel_os_hal::define_i2c_buses![
+        { name : i2c0, peripheral : I2C1, sda : PB7, scl : PB8, aliases : [SensorI2c,
+        I2C_0,] },
+    ];
 }
 #[allow(unused_variables)]
 pub fn init(peripherals: &mut ariel_os_hal::hal::OptionalPeripherals) {}
