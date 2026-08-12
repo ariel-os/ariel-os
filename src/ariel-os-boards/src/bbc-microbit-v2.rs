205332f00896aa15e582dba51275199a13107933
Antoine Lavandier <antoine.lavandier@inria.fr>
2026-08-12 16:19:27 +0200

    chore(sbd): regenerate board files
    Signed-off-by: Antoine Lavandier <antoine.lavandier@inria.fr>

--- src/ariel-os-boards/src/bbc-microbit-v2.rs
+++ src/ariel-os-boards/src/bbc-microbit-v2.rs
@@ -7,9 +7,6 @@ pub mod pins {
     ariel_os_hal::define_uarts![
         { name : uart0, device : UARTE0, tx : P0_06, rx : P1_08, host_facing : true },
     ];
-    ariel_os_hal::define_i2c_buses![
-        { name : i2c0, peripheral : TWISPI0, sda : P0_16, scl : P0_08 },
-    ];
 }
 #[allow(unused_variables)]
 pub fn init(peripherals: &mut ariel_os_hal::hal::OptionalPeripherals) {}
--- src/ariel-os-boards/src/st-nucleo-f042k6.rs
+++ src/ariel-os-boards/src/st-nucleo-f042k6.rs
@@ -3,7 +3,7 @@
 pub mod pins {
     ariel_os_hal::define_peripherals!(LedPeripherals { led0 : PA5, });
     ariel_os_hal::define_i2c_buses![
-        { name : i2c0, peripheral : I2C1, sda : PB7, scl : PB6 },
+        { name : i2c0, peripheral : I2C1, sda : PB7, scl : PB6, aliases : [] },
     ];
 }
 #[allow(unused_variables)]
