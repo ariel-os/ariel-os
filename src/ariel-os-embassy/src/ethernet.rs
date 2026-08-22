bab1e3fda9f9f715ae1bf21cf205607646299156
SimonIT <simonit.orig@gmail.com>
2026-08-22 16:51:45 +0200

    feat(net): Add generic support for WIZnet SPI Ethernet chips
    Signed-off-by: SimonIT <simonit.orig@gmail.com>

--- src/ariel-os-embassy/src/ethernet.rs
+++ src/ariel-os-embassy/src/ethernet.rs
@@ -1,2 +1,7 @@
 #[cfg(feature = "ethernet-stm32")]
 pub(crate) use crate::hal::ethernet::NetworkDevice;
+
+#[cfg(feature = "ethernet-wiznet")]
+pub(crate) mod wiznet;
+#[cfg(feature = "ethernet-wiznet")]
+pub(crate) use wiznet::NetworkDevice;
