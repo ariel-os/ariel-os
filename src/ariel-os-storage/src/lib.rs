0ae6a7044088c1cda7eaa7a7d56a0ccc00fe6b1a
Koen Zandberg <koen@bergzand.net>
2026-09-09 16:20:40 +0200

    feat(storage)!: move storage to separate NVM area
    This commit moves the storage range from the end of the rodata to a
    separate memory area allocated by ariel-os-rt.
    
    Signed-off-by: Koen Zandberg <koen@bergzand.net>

--- src/ariel-os-storage/src/lib.rs
+++ src/ariel-os-storage/src/lib.rs
@@ -5,19 +5,17 @@
 
 #![cfg_attr(not(test), no_std)]
 #![deny(missing_docs)]
-#![expect(unsafe_code)]
 // TODO: overhaul errors
 #![expect(clippy::missing_errors_doc)]
 
 mod postcard_value;
 mod storage;
 
-use core::ops::Range;
-
 use ariel_os_hal::hal::{
     OptionalPeripherals,
     storage::{Flash, FlashError, init as flash_init},
 };
+use ariel_os_rt::memory::storage_range;
 use embassy_sync::{
     blocking_mutex::raw::CriticalSectionRawMutex,
     mutex::{Mutex, MutexGuard},
@@ -31,42 +29,9 @@ static STORAGE: OnceLock<Mutex<CriticalSectionRawMutex, Storage<Flash>>> = OnceL
 const MARKER_KEY: &str = "ARIEL_INIT_MARK";
 const MARKER_VALUE: u8 = 0;
 
-/// Gets a [`Range`] from the linker that can be used for a global [`Storage`].
-///
-/// This expects two symbols `__storage_start` and `__storage_end`.
-/// This function is also the place to configure a platform dependent `OFFSET`,
-/// which configures an offset between the linker flash address map and the
-/// flash driver address map.
-fn flash_range_from_linker() -> Range<u32> {
-    #[cfg(all(context = "nrf", not(context = "nrf5340-net")))]
-    const OFFSET: usize = 0x0;
-    #[cfg(context = "nrf5340-net")]
-    const OFFSET: usize = 0x0100_0000;
-    #[cfg(context = "rp")]
-    const OFFSET: usize = 0x1000_0000;
-    #[cfg(context = "stm32")]
-    const OFFSET: usize = 0x0800_0000;
-    // Default for platform-independent tooling.
-    #[cfg(not(context = "ariel-os"))]
-    const OFFSET: usize = 0x0;
-
-    unsafe extern "C" {
-        static __storage_start: u32;
-        static __storage_end: u32;
-    }
-
-    let start = &raw const __storage_start as usize - OFFSET;
-    let end = &raw const __storage_end as usize - OFFSET;
-
-    #[expect(clippy::cast_possible_truncation)]
-    let (start, end) = (start as u32, end as u32);
-
-    start..end
-}
-
 fn init_(p: &mut OptionalPeripherals) {
     use ariel_os_log::info;
-    let flash_range = flash_range_from_linker();
+    let flash_range = storage_range();
     info!("storage: using flash range {:?}", &flash_range);
 
     let flash = flash_init(p);
