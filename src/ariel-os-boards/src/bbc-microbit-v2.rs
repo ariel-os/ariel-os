7d567da8e5b835db88ca536c963f0ba23458040d
Antoine Lavandier <antoine.lavandier@inria.fr>
2026-09-23 18:15:21 +0200

    chore: regenerate boards crate
    Signed-off-by: Antoine Lavandier <antoine.lavandier@inria.fr>

--- src/ariel-os-boards/src/bbc-microbit-v2.rs
+++ src/ariel-os-boards/src/bbc-microbit-v2.rs
@@ -7,6 +7,9 @@ pub mod pins {
     ariel_os_hal::define_uarts![
         { name : uart0, device : UARTE0, tx : P0_06, rx : P1_08, host_facing : true },
     ];
+    ariel_os_hal::define_i2c_buses![
+        { name : I2c0, peripheral : TWISPI0, sda : P0_16, scl : P0_08, aliases : [] },
+    ];
 }
 #[allow(unused_variables)]
 pub fn init(peripherals: &mut ariel_os_hal::hal::OptionalPeripherals) {}
